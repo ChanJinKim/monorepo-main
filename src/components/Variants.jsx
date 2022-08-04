af1aa5282a623a75d7653cd23c5bbded388f4d4d
ChanJin Kim <cjkim88@runbyme.team>
2022-08-04 15:36:11 +0900

    chore: monorepo-sub modules 정리

--- src/components/Variants.jsx
+++ src/components/Variants.jsx
@@ -1,13 +1,17 @@
 import * as React from 'react';
 import Skeleton from '@mui/material/Skeleton';
 import Stack from '@mui/material/Stack';
+import dayjs from 'dayjs';
 
 export default function Variants() {
   return (
-    <Stack spacing={1}>
-      <Skeleton variant="text" />
-      <Skeleton variant="circular" width={40} height={40} />
-      <Skeleton variant="rectangular" width={210} height={118} />
-    </Stack>
+    <div>
+      {dayjs().format()}
+      <Stack spacing={1}>
+        <Skeleton variant="text" />
+        <Skeleton variant="circular" width={40} height={40} />
+        <Skeleton variant="rectangular" width={210} height={118} />
+      </Stack>
+    </div>
   );
 }
